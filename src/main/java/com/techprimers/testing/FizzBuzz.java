8c261e2f834e39cf14e90c48d41813e7fa18bf50
rakeshspatil1045-droid <rakesh.spatil1045@gmail.com>
2026-03-05 20:04:19 +0530

    Add files via upload

---
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -0,0 +1,14 @@
+package com.techprimers.testing;
+
+public class FizzBuzz {
+
+    public String play(int number) {
+
+        if (number == 0) throw new IllegalArgumentException("new change to test CI pipeline");
+        if (number % 3 == 0) return "Fizz";
+        if (number % 5 == 0) return "Buzz";
+
+
+        return String.valueOf(number);
+    }
+}
